6b0c1b110adb111daffb81128dbe87b7a347a518
aselwilliams <aselwilliams22@gmail.com>
2022-05-20 13:40:28 -0400

    fetched data, added new component

--- src/App.js
+++ src/App.js
@@ -1,53 +1,60 @@
 import axios from 'axios'
-import {useState, useEffect} from 'react'
+import {useState, useEffect} from 'react';
+import Header from './components/Header'
+const animal = 'cat'; //dog, horse
+const catsURL =`https://cat-fact.herokuapp.com/facts/random?animal_type=${animal}&amount=`;
+const amount = 15
 
 function App() {
-  const [data, setData] = useState([])
+  const [cats, setCats] = useState([])
   const [localCats, setLocalCats] = useState([]);
-  const [favorites, setFavorites] = useState([])
+  const [favorites, setFavorites] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const data=[]
 
   useEffect(()=>{
-
-  })
+    // setIsLoading(true)
+axios
+  .get(`${catsURL}${amount}`)
+  .then((data)=>setCats(data))
+  console.log(cats)
+  // setIsLoading(false)
+  },[isLoading, cats])
   return (
-    <div class="main-container">
-    <header>
-        <div class="row">
-            <h1 class="title">CATS-LAND
-                <img src="https://images.pexels.com/photos/45170/kittens-cat-cat-puppy-rush-45170.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="kittens" />
-            </h1>
-        </div>
-        <div>
-            <p id="alert-msg">Some Alert</p>
-        </div>
-    </header>
+    <div className="main-container">
+    <Header />
     <main>
-       <section class="bg-color">
+       <section className="bg-color">
             <strong>Cats from API</strong>
             <a href="https://cat-fact.herokuapp.com/facts/random?animal_type=cat&amount=10">(CLICK HERE)</a>
-            <div class="api-container">
-                 <div id="fetch-data"></div>
+            <div className="api-container">
+              {cats.map((cat, i)=>
+   (<div id="fetch-data">
+                {i+1}. {cat.text}
+                </div>)
+              )}
+                 
             </div>
        </section>
-       <section class="bg-color">
-            <strong>Local Cats</strong>(add a cat fact <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-right" viewBox="0 0 16 16">
-                <path fill-rule="evenodd" d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8z"/>
+       <section className="bg-color">
+            <strong>Local Cats</strong>(add a cat fact <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-arrow-right" viewBox="0 0 16 16">
+                <path fillRule="evenodd" d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8z"/>
           </svg>)
-          <div class="local-container">
-                 <div class="local-cat"></div>
+          <div className="local-container">
+                 <div className="local-cat"></div>
           </div>
        </section>
-       <div class="third-column">
-            <section class="input-container">
+       <div className="third-column">
+            <section className="input-container">
                 <form action="#">
                     <input type="text" id="input-fact" placeholder="Add Cats Facts" />
                     <button id="add-btn">ADD</button>
                 </form>
             </section>
-            <section id="favCats" class="bg-color">
+            <section id="favCats" className="bg-color">
                 <strong>Favorite Facts</strong>
-                <div class="fav-container">
-                        <div class="fav-fact"></div>
+                <div className="fav-container">
+                        <div className="fav-fact"></div>
                 </div>
             </section>
     </div>
---
+++ src/components/Loading.js
@@ -0,0 +1,25 @@
+import React from "react";
+import "../App.css";
+
+const Loading = () => {
+  return (
+    <div>
+      <div class="lds-spinner">
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
+    </div>
+  );
+};
+
+export default Loading;
